f6d82c78ffdcaee744cf908c8030e677f4df7403
Guido Salazar Vargas <Guido.salazar.vargas7@gmail.com>
2024-09-18 18:17:46 -0400

    Ya crea un txt

--- src/springboot-generator/springboot-generator.service.ts
+++ src/springboot-generator/springboot-generator.service.ts
@@ -1,6 +1,10 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import axios from 'axios';
+import * as fs from 'fs';
 import * as stream from 'stream';
+import * as path from 'path';
+
+var AdmZip = require("adm-zip");
 
 @Injectable()
 export class SpringbootGeneratorService {
@@ -29,12 +33,35 @@ export class SpringbootGeneratorService {
         responseType: 'arraybuffer',
       });
 
+
       // Crear un stream de lectura a partir del buffer de datos
       const bufferStream = new stream.PassThrough();
       bufferStream.end(response.data);
 
-      // Devolver el stream para que pueda ser enviado como respuesta
-      return bufferStream;
+      // Descomprimir el archivo ZIP
+      const zip = new AdmZip(response.data);
+      const tempDir = path.join(__dirname, '..', '..', 'tmp', filename);
+      zip.extractAllTo(path.join(__dirname, '..', '..', 'tmp', filename), true);
+
+// Crear un archivo .txt con el contenido "Hello World!"
+const txtFilePath = path.join(tempDir, 'text.txt');
+fs.writeFileSync(txtFilePath, 'Hello World!');
+
+// Volver a comprimir el proyecto modificado
+const modifiedZip = new AdmZip();
+modifiedZip.addLocalFolder(tempDir);
+const modifiedZipBuffer = modifiedZip.toBuffer();
+
+// Crear un stream de lectura a partir del buffer de datos del ZIP modificado
+const modifiedBufferStream = new stream.PassThrough();
+modifiedBufferStream.end(modifiedZipBuffer);
+
+// Limpiar el directorio temporal
+fs.rmSync(tempDir, { recursive: true, force: true });
+
+// Devolver el stream para que pueda ser enviado como respuesta
+return modifiedBufferStream;
+
     } catch (error) {
       // Mostrar la respuesta de error decodificada
       if (error.response && error.response.data) {
